c102f680d76e28674a5735786791ecbf6d78c247
Nick Ahlers <nckahlers@gmail.com>
2023-10-24 22:24:04 -0700

    Updated Projects page and resume

--- constants/data.js
+++ constants/data.js
@@ -9,10 +9,16 @@ const userData = {
   address: "Kirkland, WA",
   projects: [
     {
-      title: "Portfolio",
-      link: "https://nickaportfolio.com",
-      imgUrl: "/pf.png",
-      description: "Professional portfolio showcasing additional projects I've built. This site was built with NextJS, TailwindCSS, Axios, and RoughNotation."
+      title: "Zumiez US",
+      link: "https://www.zumiez.com/",
+      imgUrl: "/zus.png",
+      description: "The Zumiez US site for action sport enthusiasts. Built with React, Sass, and Magento 2.",
+    },
+    {
+      title: "Zumiez Canada",
+      link: "https://www.zumiez.ca/",
+      imgUrl: "/zca.png",
+      description: "The Zumiez Canada site for action sport enthusiasts. Built with React, Sass, and Magento 2.",
     },
     {
       title: "Stormbreaker",
@@ -27,29 +33,23 @@ const userData = {
       description: "A dedicated site for fictional scenery in Washington state. Built with React, Reactstrap, Redux, Express, and MongoDB.",
     },
     {
-      title: "Zumiez US",
-      link: "http://www.zumiez.com",
-      imgUrl: "/zus.png",
-      description: "The Zumiez US site for action sport enthusiasts. Built with JavaScript, JQuery, HTML, CSS, and a Magento backend.",
+      title: "Zumiez US Legacy",
+      link: "https://web.archive.org/web/20230511123220/https://www.zumiez.com/",
+      imgUrl: "/zuslegacy.png",
+      description: "The Zumiez US legacy site. Officially sunset on October 19, 2023.",
     },
     {
-      title: "Zumiez Canada",
-      link: "http://www.zumiez.ca",
-      imgUrl: "/zca.png",
-      description: "The Zumiez Canada site for action sport enthusiasts. Built with JavaScript, JQuery, HTML, CSS, and a Magento backend.",
+      title: "Zumiez Canada Legacy",
+      link: "https://web.archive.org/web/20230501115458/https://www.zumiez.ca/",
+      imgUrl: "/zcalegacy.png",
+      description: "The Zumiez US legacy site. Officially sunset on October 19, 2023.",
     },
-    {
-      title: "Zumiez Spanish",
-      link: "http://www.zumiez.com/es_us",
-      imgUrl: "/zes.png",
-      description: "The Zumiez Spanish site for action sport enthusiasts. Built with JavaScript, JQuery, HTML, CSS, and a Magento backend.",
-    }
   ],
   about: {
     title:
       "Hi, I'm Nick Ahlers. Currently a Frontend Developer with a certification in the MERN (MongoDB, Express, React, Node) stack. I have a background in IT, which has aided in my journey in becoming a developer. I'm always looking to the future for new frameworks and libraries that offer new ways to build exciting ideas.",
     description: [
-      `Within the last few years, I've discovered my passion for all things coding. Specifically, I was drawn to React for it's declarative, robust, and adaptable library.
+      `Within the last few years, I've discovered my passion for all things coding. Specifically, I was drawn to React for it's declarative, robust, and adaptive library.
        With the power of JavaScript, I'm thrilled for the future of web innovation.`,
     ]
   },
@@ -84,7 +84,7 @@ const userData = {
     },
   ],
   resumeUrl:
-    "https://drive.google.com/file/d/1Nqn4vvRKqN-DkIdAmkVoQXafg9rHbamO/view?usp=share_link",
+    "https://drive.google.com/file/d/14776-tezvNNCeeBPRRIWknxxrluPnKV4/view?usp=drive_link",
   mernCert: 
     "https://drive.google.com/file/d/1szbtm8OkMXUx58Z2sKxJBNwfBtgFhfKs/view?usp=share_link",
   socialLinks: {
