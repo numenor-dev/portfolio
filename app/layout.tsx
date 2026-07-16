46b1921f0fbd335a9c34adeb2d9321ad2042d05f
numenor-dev <nckahlers@gmail.com>
2026-07-16 13:08:21 -0700

    Added GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body className="antialiased min-h-dvh text-zinc-300 font-sans bg-almost-black">
         <main>
           {children}
+          <GoogleTagManager gtmId="G-K6KF9S9KKX" />
         </main>
         <SpeedInsights />
       </body>
